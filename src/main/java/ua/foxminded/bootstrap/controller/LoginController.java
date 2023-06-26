46db827059bfff6c74630ef01fcbe85e12ebfd61
Antik <120013201+NewAntik@users.noreply.github.com>
2023-06-26 18:46:07 +0300

    Login controller added

--- src/main/java/ua/foxminded/bootstrap/controller/LoginController.java
+++ src/main/java/ua/foxminded/bootstrap/controller/LoginController.java
@@ -2,31 +2,12 @@ package ua.foxminded.bootstrap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import ch.qos.logback.core.model.Model;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String getLoginPage() {
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
-        String role = determineUserRole(username, password);
-
-        if (role.equals("ADMIN")) {
-            return "redirect:/admin";
-        } else if (role.equals("STUDENT")) {
-            return "redirect:/students";
-        } else if (role.equals("TEACHER")) {
-            return "redirect:/teachers";
-        }
-
         return "login";
     }
 }
