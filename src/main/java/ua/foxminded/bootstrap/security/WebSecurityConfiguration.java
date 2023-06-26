0928b8873e55a6c8f91a32354eb392b14b79417d
Antik <120013201+NewAntik@users.noreply.github.com>
2023-06-26 19:09:59 +0300

    filterChain method have been corrected

--- src/main/java/ua/foxminded/bootstrap/security/WebSecurityConfiguration.java
+++ src/main/java/ua/foxminded/bootstrap/security/WebSecurityConfiguration.java
@@ -4,27 +4,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class WebSecurityConfiguration {
+    
+    private static final String[] WHITE_LIST_URLS = {
+            "/register**",
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**"
+    };
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(authorize -> 
-         authorize.requestMatchers("/welcome").permitAll()
-             .requestMatchers("/admin/**").hasRole("ADMIN")
-             .requestMatchers("/students/**").hasRole("STUDENT")
-             .requestMatchers("/teachers/**").hasRole("TEACHER"))
+             authorize.requestMatchers(WHITE_LIST_URLS).permitAll()
+                      .requestMatchers("/admin/**").hasRole("ADMIN")
+                      .requestMatchers("/students/**").hasRole("STUDENT")
+                      .requestMatchers("/teachers/**").hasRole("TEACHER"))
         .formLogin(formLogin ->
-         formLogin.usernameParameter("username")
-             .passwordParameter("password")
-             .loginPage ("/authentication/login"))
+             formLogin.usernameParameter("username")
+                      .passwordParameter("password")
+                      .loginPage ("/register/login"))
         .logout(logout ->
-         logout.logoutUrl("/authentication/logout") 
-               .logoutSuccessUrl("/authentication/login"));
+             logout.logoutUrl("/register/logout") 
+                   .logoutSuccessUrl("/register/welcome"));
 
         return http.build();
     }
