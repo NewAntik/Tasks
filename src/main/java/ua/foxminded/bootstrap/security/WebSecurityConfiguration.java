5f8507492e4ae0779ddaa3769550e3336a57ba82
Antik <120013201+NewAntik@users.noreply.github.com>
2023-07-03 19:24:13 +0300

    HandlerMappingIntrospector bean has been added to config

--- src/main/java/ua/foxminded/bootstrap/security/WebSecurityConfiguration.java
+++ src/main/java/ua/foxminded/bootstrap/security/WebSecurityConfiguration.java
@@ -1,24 +1,36 @@
 package ua.foxminded.bootstrap.security;
 
+import java.util.Set;
+import java.util.logging.Logger;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfiguration {
-
+    private static final Logger LOGGER = Logger.getLogger(WebSecurityConfiguration.class.getName());
+    private static final String LOGIN = "/register/login";
     private static final String[] WHITE_LIST_URLS = {
-            "/register/login",
+            LOGIN,
             "/webjars/**",
             "/css/**",
             "/js/**",
             "/images/**"
     };
+    
+    @Bean(name = "mvcHandlerMappingIntrospector")
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,18 +42,43 @@ public class WebSecurityConfiguration {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers(WHITE_LIST_URLS).permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/students/**").hasRole("STUDENT")
-                                .requestMatchers("/teachers/**").hasRole("TEACHER")
-                                .requestMatchers("/course/**").hasRole("ADMIN"))
+                        .requestMatchers("/admin/**", "/welcome-admin", "/courses").hasRole("ADMIN")
+                        .requestMatchers("/students/**", "/welcome-student", "/students").hasRole("STUDENT")
+                        .requestMatchers("/teachers/**", "/welcome-teacher", "/teachers").hasRole("TEACHER")
+                        .anyRequest().authenticated())
                 .formLogin(formLogin ->
                         formLogin.passwordParameter("password")
-                                .loginPage("/register/login")
-                                .defaultSuccessUrl("/welcome", true))
+                                 .loginPage(LOGIN)
+                                 .defaultSuccessUrl("/", true)
+                                 .successHandler(successHandler()))
                 .logout(logout ->
                         logout.logoutUrl("/register/logout")
-                                .logoutSuccessUrl("/register/welcome"));
+                              .logoutSuccessUrl(LOGIN)
+                              .invalidateHttpSession(true)
+                              .deleteCookies("JSESSIONID"));
 
         return http.build();
+    }
+    
+    private AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+            if (roles.contains("ROLE_ADMIN")) {
+                LOGGER.info("Redirecting to /welcome-admin");
+                response.sendRedirect("/welcome-admin");
+            } else if (roles.contains("ROLE_STUDENT")) {
+                LOGGER.info("Redirecting to /welcome-student");
+                response.sendRedirect("/welcome-student");
+            } else if (roles.contains("ROLE_TEACHER")) {
+                LOGGER.info("Redirecting to /welcome-teacher");
+                response.sendRedirect("/welcome-teacher");
+            } else if (roles.contains("ROLE_STUFF")) {
+                LOGGER.info("Redirecting to /welcome-stuff");
+                response.sendRedirect("/welcome-stuff");
+            } else {
+                response.sendRedirect(LOGIN);
+            }
+        };
     }
 }
