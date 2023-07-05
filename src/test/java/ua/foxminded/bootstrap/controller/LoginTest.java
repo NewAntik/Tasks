662b251bef639ffc8278c2f9da7bc0c533137422
Antik <120013201+NewAntik@users.noreply.github.com>
2023-07-05 16:25:51 +0300

    test methods have been corrected it loginTest.class

--- src/test/java/ua/foxminded/bootstrap/controller/LoginTest.java
+++ src/test/java/ua/foxminded/bootstrap/controller/LoginTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import ua.foxminded.bootstrap.models.Group;
@@ -19,9 +18,6 @@ import ua.foxminded.bootstrap.security.SecuritySharedConfiguration;
 import ua.foxminded.bootstrap.security.WebSecurityConfiguration;
 import ua.foxminded.bootstrap.service.UserService;
 
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -53,27 +49,22 @@ class LoginTest {
     @Test
     void shouldLoginStudentWithCorrectUsernameAndPassword() throws Exception {
         Student user = new Student("student", passwordEncoder.encode("1234"), Role.STUDENT, "Student", "Student", new Group("AA-01"));
-//        when(userService.findByLogin("student")).thenReturn(Optional.of(user));
+        when(userService.loadUserByUsername("student")).thenReturn(new MyUserDetails(user));
 
         mvc.perform(formLogin("/register/login").user("username", user.getLogin()).password("1234"))
            .andExpect(status().is3xxRedirection())
-           .andExpect(header().string("Location", "/welcome"));
+           .andExpect(header().string("Location", "/welcome-student"));
     }
 
     @Test
     void shouldNotLoginUserWithBadPassword() throws Exception {
-        User user = new User("test", passwordEncoder.encode("secret"), Role.ADMIN, "Test", "Testerson");
-//        when(userService.findByLogin("test")).thenReturn(Optional.of(user));
-
-        mvc.perform(formLogin("/register/login").user("username", user.getLogin()).password("1234"))
+        mvc.perform(formLogin("/register/login").user("username", "test").password("1234"))
            .andExpect(status().is3xxRedirection())
            .andExpect(header().string("Location", "/register/login?error"));
     }
 
     @Test
     void shouldNotLoginUserWithBadLogin() throws Exception {
-//        when(userService.findByLogin(anyString())).thenReturn(Optional.empty());
-
         mvc.perform(formLogin("/register/login").user("username", "test").password("1234"))
            .andExpect(status().is3xxRedirection())
            .andExpect(header().string("Location", "/register/login?error"));
