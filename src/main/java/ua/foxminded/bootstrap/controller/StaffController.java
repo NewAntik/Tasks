1d4ca6aa7a83a897d67b50b4095349e316c37439
Antik <120013201+NewAntik@users.noreply.github.com>
2023-07-13 14:13:17 +0300

    method getStaffWelcome has been added to StaffController class

--- src/main/java/ua/foxminded/bootstrap/controller/StaffController.java
+++ src/main/java/ua/foxminded/bootstrap/controller/StaffController.java
@@ -1,15 +1,13 @@
 package ua.foxminded.bootstrap.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StaffController {
 
-    
     @GetMapping("/welcome-staff")
     public String getStaffWelcome(){
         return "staff/welcome";
-    }
+    } 
 }
--- src/test/java/ua/foxminded/bootstrap/controller/StaffControllerTest.java
+++ src/test/java/ua/foxminded/bootstrap/controller/StaffControllerTest.java
@@ -1,25 +1,31 @@
 package ua.foxminded.bootstrap.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.foxminded.bootstrap.security.WebSecurityConfiguration;
+import ua.foxminded.bootstrap.service.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = {StaffController.class})
+@Import(WebSecurityConfiguration.class)
 class StaffControllerTest {
 
     @Autowired
     MockMvc mvc;
     
+    @MockBean
+    UserService userService;
+    
     @Test
-    void getStaffPage_ShouldRedirectToLoginPage() throws Exception {
+    void getStaffWelcome_ShouldRedirectToLoginPage() throws Exception {
         mvc.perform(get("/welcome-staff"))
         .andExpect(status().is3xxRedirection())
         .andExpect(header().string("Location", "http://localhost/register/login"));
@@ -27,9 +33,8 @@ class StaffControllerTest {
     
     @Test
     @WithMockUser(roles = "TEACHER")
-    void shouldDenyAccessWithWrongRole() throws Exception {
+    void getStaffWelcome_ShouldDenyAccessWithWrongRole() throws Exception {
         mvc.perform(get("/welcome-staff"))
                 .andExpect(status().isForbidden());
     }
-
 }
